ea3bc99cc1ad086acd8f7e263d10175f51db8853
Torin Blankensmith <torin.blankensmith@gmail.com>
2021-06-06 17:27:48 -0400

    default to createSculpture instead of custom geometry

--- es6-three-starter-template/index.js
+++ es6-three-starter-template/index.js
@@ -7,7 +7,7 @@ let scene = new Scene();
 let params = { time: 0 };
 
 let camera = new PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.z = 60;
+camera.position.z = 4;
 
 let renderer = new WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -17,16 +17,23 @@ renderer.setClearColor( new Color(1, 1, 1), 1 );
 document.body.appendChild( renderer.domElement );
 
 
-let geometry = new TorusKnotGeometry( 10, 3, 192, 96);
+let geometry = new TorusKnotGeometry( 1, .3, 100, 9.6);
 geometry.computeBoundingSphere();
 geometry.center();
 
 
-//Shader Park Setup
-let mesh = createSculptureWithGeometry(geometry, spCode, () => ( {
-  time: params.time,
+// Shader Park Setup
+let mesh = createSculpture(spCode, () => ( {
+    time: params.time,
 } ));
 scene.add(mesh);
+
+// *** Uncomment to try using a custom geometry. Make sure to comment out likes 26-29 ***.
+
+// let mesh = createSculptureWithGeometry(geometry, spCode, () => ( {
+//   time: params.time,
+// } ));
+// scene.add(mesh);
 
 let controls = new OrbitControls( camera, renderer.domElement, {
   enableDamping : true,
