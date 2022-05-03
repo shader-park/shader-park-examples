87d0163af5ddda91692d449c89b92af99b278c04
Torin Blankensmith <torin.blankensmith@gmail.com>
2022-05-03 17:05:37 -0400

    upgrade to latest sp-core + new starter template

--- es6-starter-template/spCode.js
+++ es6-starter-template/spCode.js
@@ -5,5 +5,14 @@
 
 export function spCode()  {
   // Put your Shader Park Code here
-  sphere(.5);
+  rotateY(mouse.x * PI / 2 + time*.5);
+  rotateX(mouse.y * PI / 2);
+  metal(.5);
+  shine(.4);
+  color(getRayDirection()+.2);
+  rotateY(getRayDirection().y*4+time)
+  boxFrame(vec3(.4), .02);
+  expand(.02);
+  blend(nsin(time)*.6)
+  sphere(.2);
 };
